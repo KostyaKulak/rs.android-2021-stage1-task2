9b6da93a8bcdd0e1eb4f4b7c1084e2631dec8688
Kanstantsin Kulak <kkulak24@gmail.com>
2021-06-19 08:26:51 +0200

    feat: complete Blocks

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,25 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        val selectedOfClassB = mutableListOf<Any>()
+        for (a in blockA) {
+            if (a!!::class == blockB) {
+                selectedOfClassB.add(a)
+            }
+        }
+        return when (blockB) {
+            Int::class -> selectedOfClassB.sumBy { it as Int }
+            String::class -> selectedOfClassB.joinToString("")
+            LocalDate::class -> selectedOfClassB.map { it as LocalDate }
+                .reduce { x, y -> if (x.isAfter(y)) x else y }.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            else -> ""
+        }
     }
 }
