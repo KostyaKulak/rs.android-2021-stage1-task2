e1967f62c5c7d88c18c88eedc48550e7ff5dc4b8
Kanstantsin Kulak <kkulak24@gmail.com>
2021-06-19 08:13:05 +0200

    feat: complete Abbreviation

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,34 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        if (a.equals(b, ignoreCase = true)) {
+            return "YES"
+        }
+        if (b.length > a.length) {
+            return "NO"
+        }
+        var result = "YES"
+        val charsA = a.toCharArray()
+        val charsB = b.toCharArray()
+        var offset = 0
+        var remainder = ""
+        for (i in charsB.indices) {
+            while (i + offset < charsA.size - 1)
+                if (!charsA[i + offset].equals(charsB[i], ignoreCase = true)) {
+                    offset++
+                } else {
+                    break
+                }
+            if (i + offset > charsA.size - 1 || !charsA[i + offset].equals(charsB[i], ignoreCase = true)) {
+                result = "NO"
+            }
+            if (i == charsB.size - 1) {
+                remainder += charsA.slice(i + offset + 1 until charsA.size).joinToString()
+            }
+        }
+        if (remainder.toLowerCase() != remainder) {
+            result = "NO"
+        }
+        return result
     }
 }
